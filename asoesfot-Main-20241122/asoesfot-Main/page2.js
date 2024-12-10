305712e440a547bcd22d590bda8d8bbc8c013eba
Jose-Vargas28 <168014982+Jose-Vargas28@users.noreply.github.com>
2024-12-10 09:34:19 -0500

    Add files via upload

---
+++ asoesfot-Main-20241122/asoesfot-Main/page2.js
@@ -0,0 +1,24 @@
+alert("BIENVENIDO A LA ESFOT")
+
+var typed = new Typed('.element', {
+    strings: ["First sentence.", "Second sentence."],
+    typeSpeed: 30
+  });
+var n1=1;
+var n2=2;
+
+var suma;
+suma=n1+n2;
+
+
+alert("La suma es: " +suma);
+
+let l1=4;
+let l2=3;
+
+let sum;
+sum=l1+l2;
+
+
+
+alert("La suma es: " +sum);
